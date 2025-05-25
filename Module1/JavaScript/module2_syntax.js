3ea72eb9c7a5448184ecc975d7f40e6388ce2b42
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-25 20:49:37 +0530

    Create module2_syntax.js

---
+++ Module1/JavaScript/module2_syntax.js
@@ -0,0 +1,8 @@
+const eventName = "Art Fair";
+const eventDate = "2025-06-20";
+let availableSeats = 30;
+
+console.log(`${eventName} is scheduled for ${eventDate}. Available seats: ${availableSeats}`);
+
+availableSeats--;
+console.log(`After one registration, seats left: ${availableSeats}`);
