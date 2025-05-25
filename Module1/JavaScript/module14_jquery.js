f97426e49eba2a65e6788b6a46d506e5079f0faf
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-25 21:02:22 +0530

    Create module14_jquery.js

---
+++ Module1/JavaScript/module14_jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#registerBtn').click(function () {
+        alert('Registered via jQuery!');
+    });
+
+    $('.event-card').fadeIn(1000).fadeOut(1000);
+});
+
+// Advantage of React/Vue:
+// Components are reusable, reactive, and easier to manage with larger apps.
