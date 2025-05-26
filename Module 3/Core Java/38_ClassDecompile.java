12a8db4f63f4362e8773236c49d825c56eb653f8
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-26 09:27:47 +0530

    Create 38_ClassDecompile.java

---
+++ Module 3/Core Java/38_ClassDecompile.java
@@ -0,0 +1,14 @@
+// 38_ClassDecompile.java
+public class ClassDecompile {
+    public void demoMethod() {
+        System.out.println("This method is for decompiling practice.");
+    }
+    
+    public static void main(String[] args) {
+        new ClassDecompile().demoMethod();
+    }
+}
+
+/*
+Compile this class and use a decompiler tool (like JD-GUI or CFR) to view the source code.
+*/
