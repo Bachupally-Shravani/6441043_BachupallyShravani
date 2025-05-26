8034ea7f62bfa17c7f9fd7f1ba7192c1102b7204
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-26 09:19:41 +0530

    Create 19_InterfaceImplementation.java

---
+++ Module 3/Core Java/19_InterfaceImplementation.java
@@ -0,0 +1,25 @@
+// 19_InterfaceImplementation.java
+interface Playable {
+    void play();
+}
+
+class Guitar implements Playable {
+    public void play(){
+        System.out.println("Playing the Guitar.");
+    }
+}
+
+class Piano implements Playable {
+    public void play(){
+        System.out.println("Playing the Piano.");
+    }
+}
+
+public class InterfaceImplementation {
+    public static void main(String[] args) {
+        Playable guitar = new Guitar();
+        Playable piano = new Piano();
+        guitar.play();
+        piano.play();
+    }
+}
