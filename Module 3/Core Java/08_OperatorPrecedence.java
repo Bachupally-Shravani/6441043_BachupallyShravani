8098501bc7588fa35e7fcf1fec8cc2466502b3df
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-26 09:16:04 +0530

    Create 08_OperatorPrecedence.java

---
+++ Module 3/Core Java/08_OperatorPrecedence.java
@@ -0,0 +1,9 @@
+// 08_OperatorPrecedence.java
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        int result = 10 + 5 * 2; // Multiplication has precedence over addition.
+        System.out.println("10 + 5 * 2 = " + result);
+        // You could add more complex expressions and explain:
+        // For example: int result2 = (10 + 5) * 2; would force addition before multiplication.
+    }
+}
