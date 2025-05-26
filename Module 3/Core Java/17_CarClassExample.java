926887db564b74a3847c851c3099d8d400f70283
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-26 09:18:52 +0530

    Create 17_CarClassExample.java

---
+++ Module 3/Core Java/17_CarClassExample.java
@@ -0,0 +1,25 @@
+// 17_CarClassExample.java
+class Car {
+    String make;
+    String model;
+    int year;
+    
+    Car(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    
+    void displayDetails(){
+        System.out.println("Car Details: " + year + " " + make + " " + model);
+    }
+}
+
+public class CarClassExample {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Camry", 2020);
+        Car car2 = new Car("Honda", "Civic", 2019);
+        car1.displayDetails();
+        car2.displayDetails();
+    }
+}
