3a7cffd645daa6745ccc188b5126e72fcb06e9c7
Bachupally-Shravani <bachupally.0123@gmail.com>
2025-05-26 09:14:49 +0530

    Create 06_DataTypeDemo.java

---
+++ Module 3/Core Java/06_DataTypeDemo.java
@@ -0,0 +1,16 @@
+// 06_DataTypeDemo.java
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int intVar = 100;
+        float floatVar = 12.34f;
+        double doubleVar = 56.789;
+        char charVar = 'A';
+        boolean boolVar = true;
+        
+        System.out.println("Integer: " + intVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Char: " + charVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+}
